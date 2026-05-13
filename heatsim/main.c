f0a77ef045bc7b33289ded3a99fc10cf6715ec28
dobozi <botond.dobozi@student.uni-miskolc.hu>
2026-05-13 15:36:13 +0200

    aaaaa

--- heatsim/main.c
+++ heatsim/main.c
@@ -6,7 +6,9 @@
 // Makró: fordítás előtt behelyettesített szövegrész; N: szélesség; i,j: koordináták
 #define INDEX(i, j, N) ((i) * (N) + (j)) // Egy 1D tömböt 2D mátrixként kezel: sorindex * szélesség + oszlopindex
 
+//------------------------------------------------------------------------------------------------
 
+// INICIALIZÁLÁS: A mátrix alaphelyzetbe állítása
 
 // matrix: mutató a memóriaterületre; N: mátrix oldalhossza
 void initialize(double* matrix, int N) { // void: nincs visszatérési érték; double*: tizedestört mutató
@@ -18,14 +20,16 @@ void initialize(double* matrix, int N) { // void: nincs visszatérési érték; 
     matrix[INDEX(N/2, N/2, N)] = 100.0; // fix hőforrás beállítása a mátrix közepén
 }
 
+//------------------------------------------------------------------------------------------------
 
+// JACOBI ITERÁCIÓ: A párhuzamosított számítási mag
 
 // current/next: két puffer az állapotoknak; iterations: ciklusok száma; threads: magok száma
 // sched/chunk_size: OpenMP specifikus paraméterek a munka elosztásához
 void jacobi_iteration(double* current, double* next, int N,
                       int iterations, int threads,
                       omp_sched_t sched, int chunk_size) {
-    omp_set_num_threads(threads);       // omp_set_num_threads: megadja az OpenMP-nek, hány szálat indítson el
+    omp_set_num_threads(threads);        // omp_set_num_threads: megadja az OpenMP-nek, hány szálat indítson el
     omp_set_schedule(sched, chunk_size); // beállítja az ütemezési stratégiát (hogyan ossza szét a sorokat)
 
     for (int iter = 0; iter < iterations; ++iter) {  // időbeli iteráció: ennyiszer frissítjük a teljes mátrixot
@@ -50,25 +54,27 @@ void jacobi_iteration(double* current, double* next, int N,
     }
 }
 
+//------------------------------------------------------------------------------------------------
 
+// MAIN FÜGGVÉNY: Paraméterek beolvasása, memóriakezelés és mérés
 
 int main(int argc, char* argv[]) { // argc: paraméterek száma; argv: paraméterek szöveges tömbje
+    
+    // Argumentum ellenőrzés
     if (argc != 6) {               // ha nem pontosan 5 paramétert (plusz a program neve) kapott
         printf("Használat: %s <matrix_meret> <iteraciok> <szalak> <schedule_tipus> <chunk_meret>\n", argv[0]);
         return 1;                  // hiba jelzése az operációs rendszer felé
     }
 
-
-
-    int N = atoi(argv[1]);         // atoi: karakterláncból egész számot csinál (mátrix méret)
+    // Paraméterek feldolgozása
+    int N = atoi(argv[1]);          // atoi: karakterláncból egész számot csinál (mátrix méret)
     int iterations = atoi(argv[2]); // iterációk száma
     int threads = atoi(argv[3]);    // használandó szálak száma
     char* sched_type_str = argv[4]; // ütemezési mód neve szövegként
     int chunk_size = atoi(argv[5]); // darabolási méret (hány sor jusson egyszerre egy szálnak)
 
-
-
-    omp_sched_t schedule;          // OpenMP belső típusa az ütemezéshez
+    // Ütemezési mód beállítása
+    omp_sched_t schedule;           // OpenMP belső típusa az ütemezéshez
     // strcmp: összehasonlítja a bemenetet a kulcsszavakkal; 0 értéket ad, ha egyeznek
     if (strcmp(sched_type_str, "static") == 0) schedule = omp_sched_static; // fix kiosztás
     else if (strcmp(sched_type_str, "dynamic") == 0) schedule = omp_sched_dynamic; // sorban kérik a szálak a munkát
@@ -77,8 +83,7 @@ int main(int argc, char* argv[]) { // argc: paraméterek száma; argv: paraméte
         return 1; // ismeretlen paraméter esetén kilépés
     }
 
-
-
+    // Memória foglalása
     // malloc: memóriafoglalás a heap-en; sizeof(double): egy szám mérete bájtokban; N*N: összes elem
     double* current = (double*)malloc(N * N * sizeof(double)); // (double*): kényszerített típusátalakítás (cast)
     double* next = (double*)malloc(N * N * sizeof(double));    // puffer a számítások eredményének
@@ -87,21 +92,18 @@ int main(int argc, char* argv[]) { // argc: paraméterek száma; argv: paraméte
         return 1;
     }
 
-
-
+    // Inicializálás
     initialize(current, N);        // kezdőállapot beállítása
     initialize(next, N);           // célpuffer alaphelyzetbe állítása
 
-
-
+    // Időmérés és futtatás
     double start = omp_get_wtime(); // omp_get_wtime: "falóra" idő, másodpercben adja vissza a pontos időt
     jacobi_iteration(current, next, N, iterations, threads, schedule, chunk_size); // Fő számítási blokk
     double end = omp_get_wtime();   // mérés vége
     double elapsed = end - start;   // kivonás: megkapjuk a futási időt
     printf("Time taken with %d threads (%s): %.6f seconds\n", threads, sched_type_str, elapsed);
 
-
-
+    // Eredmények fájlba írása
     FILE* f = fopen("results.csv", "a"); // fopen: fájl megnyitása; "a": append (hozzáfűzés a fájl végéhez)
     if (f) {
         // fprintf: formázott írás fájlba; CSV formátum: vesszővel elválasztott értékek
@@ -110,8 +112,7 @@ int main(int argc, char* argv[]) { // argc: paraméterek száma; argv: paraméte
         fclose(f);                  // fclose: fájl lezárása és puffer ürítése a lemezre
     }
 
-
-
+    // Felszabadítás
     free(current); // free: a malloc-al foglalt memória felszabadítása (visszaadjuk a rendszernek)
     free(next);    // felszabadítás a memóriaszivárgás megelőzésére
     return 0;      // sikeres befejezés
